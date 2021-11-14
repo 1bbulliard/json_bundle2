1176f85e9b1c5dcc12f5e2d5107d3278a7bb2a8d
bbulliard <bbulliard@gmail.com>
2021-11-14 16:05:05 -0600

    Initial Commit

---
+++ Shared/json_bundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_bundleApp.swift
+//  Shared
+//
+//  Created by Bob Bulliard on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct json_bundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
