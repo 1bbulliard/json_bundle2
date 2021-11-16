c6ff76e73b343baf2d605cda3b61d280a4f891f7
bbulliard <bbulliard@gmail.com>
2021-11-16 17:00:34 -0600

    20211116_1700_works

--- ContentView.swift
+++ ContentView.swift
@@ -42,11 +42,21 @@ struct ContentView: View
                  List{
                      NavigationLink("List Json", destination: nextview())
                         .padding()
-                     NavigationLink("choos world2", destination: nextview2())
+                     NavigationLink("list only name", destination: nextview2())
                          .padding()
                      NavigationLink("choos world3", destination: nextview3(users: (users)))
                          .padding()
+                     NavigationLink("choose Tinas world4", destination: nextview4())
+                     Image("tina")
+                         .renderingMode(.original)
+                         .resizable()
+                         .frame(width: 100, height: 100)
+                         .padding()
+                     NavigationLink("App storage- remembers whats typed", destination: nextview5())
+                        .padding()
+                     
                  }
+                 
              }}
      // end nav link}
     struct nextview: View
@@ -86,19 +96,36 @@ struct ContentView: View
   }
 }
 struct nextview2: View {
+ //   @State private var usersx: User = users()
     var body: some View {
      
-        Text("Hello, world2!")
+        Text("list only name!")
         
             .font(.callout)
             .fontWeight(.bold)
             .padding(60.0)
-           
-            .background(.green)
+           .background(.green)
+        
+        
+        List{
+            ForEach(users) {user in
+                    Text(user.name)
+            }
+            // begin delete
+            .onDelete { indexSet in
+            for index in indexSet {
+                    users.remove(at: index)}}
+            // end delete
             
-    }
+            .onMove (perform: move)
+            // begin move
+            // end move
+         //   .navigationBarItems(trailing: EditButton())
+        }}}
+func move(from source: IndexSet, to destination : Int) {
+    
+    users.move(fromOffsets: source, toOffset: destination)
 }
-
 struct nextview3: View {
    // var users : User
     var users:[User]
@@ -113,4 +140,58 @@ struct nextview3: View {
             .background(.yellow)
        
     }
+}
+struct ResultView: View {
+    var choice: String
+
+    var body: some View {
+        Text("You chose \(choice)")
+    }
+}
+struct nextview4: View {
+   
+    var body: some View {
+        
+        Text("Tina's flip of the coin!")
+            .font(.callout)
+            .fontWeight(.bold)
+            .padding(60.0)
+            .background(.yellow)
+        
+        NavigationView {
+                    VStack(spacing: 30) {
+                        Text("You're going to flip a coin – do you want to choose heads or tails?")
+
+                        NavigationLink(destination: ResultView(choice: "Heads")) {
+                            Text("Choose Heads")
+                        }
+
+                        NavigationLink(destination: ResultView(choice: "Tails")) {
+                            Text("Choose Tails")
+                        }
+                    }
+                    .navigationTitle("Navigation")
+                }
+       
+    }
+}
+struct nextview5: View {
+    
+    @AppStorage("firstname") var firstname = ""
+    @AppStorage("lastname") var lastname = ""
+    @AppStorage("issubscribed") var issubscribed = false
+    
+    var body: some View {
+        VStack{
+            Form{
+                Section(header: Text("Details")) {
+                TextField("First Name", text: $firstname)
+                TextField("Last Name", text: $lastname)
+                    Toggle("is subscribed",isOn: $issubscribed)
+        }
+                
+            }
+        .navigationTitle("AppStorage- remembers whats typed")
+}
+}
 }
